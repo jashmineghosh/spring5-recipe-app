4d0a54ea618c39030d0c962ed7bdc1ce03fef0fe
emb-jashbhu <jashmine.bhuiya.extern@diconium.com>
2023-01-05 14:42:27 +0530

    bi-directional relation

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -3,6 +3,7 @@ package guru.springframework.domain;
 import org.hibernate.engine.internal.Cascade;
 
 import javax.persistence.*;
+import java.util.HashSet;
 import java.util.Set;
 
 @Entity
@@ -27,13 +28,13 @@ public class Recipe {
     private Notes notes;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
-    private Set<Ingredient> ingredients;
+    private Set<Ingredient> ingredients = new HashSet<>();
 
     @ManyToMany
     @JoinTable(name = "recipe_category",
             joinColumns = @JoinColumn(name = "recipe_id"),//The joinColumn attribute will connect to the owner side of the relationship, and the inverseJoinColumn to the other side
             inverseJoinColumns = @JoinColumn(name = "category_id"))//this is foreign key on owning side
-    private Set<Category> categories;
+    private Set<Category> categories = new HashSet<>();;
 
     public Long getId() {
         return id;
@@ -121,6 +122,12 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+        notes.setRecipe(this);
+    }
+    public Recipe addIngredient(Ingredient ingredient){
+        ingredient.setRecipe(this);
+        this.ingredients.add(ingredient);
+        return this;
     }
     public Set<Ingredient> getIngredients() {
         return ingredients;
@@ -130,4 +137,11 @@ public class Recipe {
         this.ingredients = ingredients;
     }
 
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
+    }
 }
